3b97871dded8f6742a9cecf11ee3ed94af7b884f
omartinez1204 <omar1204@gmail.com>
2021-01-18 11:07:49 -0600

    Agregué una nueva vista

--- esnova/views.py
+++ esnova/views.py
@@ -4,3 +4,6 @@ from django.template import Template, Context
 from django.shortcuts import render
 def index(request):
     return HttpResponse("<h1>EsNova<br><hr></h1>")
+
+def primerView(request):
+    pass
