ba41454a015defb8ee306d4b219572fb32adc59f
EymarRamirez <EymarRamirez@users.noreply.github.com>
2021-01-19 20:20:30 -0600

    Modelo gastosDelSolicitante

--- serviciosapp/models.py
+++ serviciosapp/models.py
@@ -24,7 +24,33 @@ class datosPersonales(models.Model):
     propietario = models.CharField(max_length = 30)
     parentesco = models.CharField(max_length = 30)
     def __str__(self):
-        return '{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}{12}{13}{14{15}{16}{17}{18}{19}{20}'.format(self.fecha, self.solicita_beca_alimentaria, self.ap_paterno, self.ap_materno, self.nombre
-        , self.sexo, self.edad, self.estado_civil, self.carrera, self.semestre, self.grupo, self.telefono
-        , self.otro_idiona, self.residencia_distinta, self.calle, self.numero, self.colonia, self.municipio
-        , self.estado, self.propietario, self.parentesco)
+        return '{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}{12}{13}{14{15}{16}{17}{18}{19}{20}'.format(self.fecha, self.solicita_beca_alimentaria,
+        self.ap_paterno, self.ap_materno, self.nombre, self.sexo, self.edad, self.estado_civil, self.carrera, self.semestre, self.grupo,
+        self.telefono, self.otro_idiona, self.residencia_distinta, self.calle, self.numero, self.colonia, self.municipio, self.estado,
+        self.propietario, self.parentesco)
+
+class gastosDelSolicitante(models.Model):
+    gastos_mensuales = models.FloatField()
+    gastos_adicionales = models.FloatField()
+    cant_personas_renta = models.IntegerField(default=0)
+    renta_mensual = models.FloatField()
+    cant_familiares_renta = models.IntegerField(default=0)
+    parentesco_arrentador = models.CharField(max_length = 30)
+    medio_de_transporte = models.CharField(max_length = 30)
+    datos_transporte_propio = models.CharField(max_length = 30)
+    celular = models.BooleanField()
+    celular_marca = models.CharField(max_length = 30)
+    celular_modelo = models.CharField(max_length = 30)
+    camara_fotografica = models.BooleanField()
+    reproductor_de_audio = models.BooleanField()
+    tableta_electronica = models.BooleanField()
+    centro_de_trabajo = models.CharField(max_length = 30)
+    ingreso_mensual = models.FloatField()
+    datos_trabajo = models.CharField(max_length = 30)
+    jefe_de_familia = models.BooleanField()
+    personas_dependientes = models.IntegerField(default=0)
+    def __str__(self):
+        return '{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}{12}{13}{14{15}{16}{17}{18}{19}'.format(self.gastos_mensuales, self.gastos_adicionales,
+        self.cant_personas_renta, self.renta_mensual, self.cant_familiares_renta, self.parentesco_arrentador, self.medio_de_transporte,
+        self.datos_transporte_propio, self.celular, self.celular_marca, self.celular_modelo,self.camara_fotografica, self.reproductor_de_audio,
+        self.tableta_electronica, self.centro_de_trabajo, self.ingreso_mensual, self.datos_trabajo, self.jefe_de_familia, self.personas_dependientes)
