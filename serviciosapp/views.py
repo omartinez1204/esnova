e403cbb4be8bab20f05978dfb2ec73f9d14fd07d
Ricardo Martin Gonzalez Diaz <richimartin99@users.noreply.github.com>
2021-01-23 22:28:21 -0600

    views login

--- serviciosapp/views.py
+++ serviciosapp/views.py
@@ -6,7 +6,19 @@ from serviciosapp.models import *
 # Create your views here.
 
 def login(request):
-    matricula=request.GET['matricula']
-    contrasenia=request.GET['contrasenia']
-    
-    return render(request, 'datos_solicitante.html')
+    matricula=request.POST['matricula']
+    contrasenia=request.POST['contrasenia']
+
+    usuarios = usuario.objects.all()
+    for usuario in usuarios:
+        if usuario.matricula == matricula and usuario.contrasenia == contrasenia
+            u = usuario
+    context = {
+        'usuario':u
+    }
+    return render(request, 'datos_solicitante.html', context)
+
+def registrar(request):
+    u = usuario(request.POST['matricula'], request.POST['contrasenia'], request.POST['email'])
+    u.save()
+    return render(request, 'index.html')
