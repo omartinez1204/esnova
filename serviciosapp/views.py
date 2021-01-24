b4fbf572bb4bd174a4d0444b2804872cef1e1bd6
eroseli <48666884+eroseli@users.noreply.github.com>
2021-01-24 06:24:40 -0600

    nuevo

--- serviciosapp/views.py
+++ serviciosapp/views.py
@@ -11,7 +11,7 @@ def login(request):
     contrasenia=request.POST['contrasenia']
     usuarios = usuario.objects.all()
     for usuario in usuarios:
-        if usuario.matricula == matricula and usuario.contrasenia == contrasenia
+        if usuario.matricula == matricula and usuario.contrasenia == contrasenia:
             u = usuario
     context = {
         'usuario':u
