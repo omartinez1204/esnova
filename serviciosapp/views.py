34249a40f178a7f54688d10f8a5450e800d17ae8
eroseli <48666884+eroseli@users.noreply.github.com>
2021-01-24 05:07:03 -0600

    Corrección views

--- serviciosapp/views.py
+++ serviciosapp/views.py
@@ -7,18 +7,8 @@ from serviciosapp.models import *
 # Create your views here.
 
 def login(request):
-<<<<<<< HEAD
-    matricula=request.GET['matricula']
-    contrasenia=request.GET['contrasenia']
-
-    return render(request, 'datos_solicitante.html')
-
-def obtenerDatos_Solicitante(request):
-    return render(request,'index.html')
-=======
     matricula=request.POST['matricula']
     contrasenia=request.POST['contrasenia']
-
     usuarios = usuario.objects.all()
     for usuario in usuarios:
         if usuario.matricula == matricula and usuario.contrasenia == contrasenia
@@ -32,4 +22,7 @@ def registrar(request):
     u = usuario(request.POST['matricula'], request.POST['contrasenia'], request.POST['email'])
     u.save()
     return render(request, 'index.html')
->>>>>>> d46141bbddd3e5bfab1f4355505acb3836c13e96
+
+
+def obtenerDatos_Solicitante(request):
+    return render(request,'index.html')
