60b229f2e68babe24d72fe01ed01d2da110ff25f
eroseli <48666884+eroseli@users.noreply.github.com>
2021-01-25 02:11:05 -0600

    Actualizacion

--- serviciosapp/views.py
+++ serviciosapp/views.py
@@ -3,6 +3,7 @@ import datetime
 from django.template import Template, Context
 from django.shortcuts import render
 from serviciosapp.models import *
+from datetime import datetime,date
 
 # Create your views here.
 
@@ -25,15 +26,27 @@ def login(request):
 
 
 def obtenerDatos_Solicitante(request):
-    datosPersonalesModel = datosPersonales(solicita_beca_alimentaria= bool(request.POST['solicita_beca_alimentaria']),
-        ap_paterno=request.POST(['apellido_paterno']), ap_materno = request.POST(['apellido_paterno']), nombre = request.POST(['nombre']),
-        sexo = request.POST(['flexRadioDefault']), edad =  int(request.POST(['edad'])) , estado_civil = request.POST(['estado_civi']), carrera =request.POST(['carrera']),
-        semestre = int(request.POST(['semestre'])),grupo = request.POST(['grupo']),celular = request.POST(['celular']), otro_idiona =  request.POST(['otro_idioma']),
-        residencia_distinta =  bool(request.POST(['residencia_distinta'])), calle =  request.POST(['calle']), numero =  int(request.POST(['numero_calle'])),
-        colonia = request.POST(['barrio']), municipio = request.POST(['municipio']), estado = request.POST(['estado']),propietario = request.POST(['propietario']),
-        parentesco = request.POST(['parentesco']))
+
+    if request.POST['solicita_beca_alimentaria'] == 'si':
+        variable = True
+    else :
+        variable = False
+
+    if request.POST['residencia_distinta'] == 'si':
+        variable2 = True
+    else :
+        variable2 = False
+
+
+    datosPersonalesModel = datosPersonales(fecha = date.today(), solicita_beca_alimentaria= bool(variable),
+    ap_paterno=request.POST['apellido_paterno'], ap_materno = request.POST['apellido_materno'], nombre = request.POST['nombre'],
+    sexo = request.POST['sexo'], edad = request.POST['edad'], estado_civil = request.POST['estado_civil'], carrera =request.POST['carrera'],
+    semestre = request.POST['semestre'],grupo = request.POST['grupo'],telefono = request.POST['celular'], otro_idiona =  request.POST['otro_idioma'],
+    residencia_distinta = variable2 , calle =  request.POST['calle'], numero = request.POST['numero_calle'],
+    colonia = request.POST['barrio'], municipio = request.POST['municipio'], estado = request.POST['estado'],propietario = request.POST['propietario'],
+    parentesco = request.POST['parentesco'])
+
     context = {
         'datosPersonalesModel': datosPersonalesModel,
     }
-
-#    return render(request,'temporal.html',context)
+    return render(request,'temporal.html',context)
