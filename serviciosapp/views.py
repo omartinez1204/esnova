7208fc7e779e0c0243f30a20b45b62b5c90d9f3d
Ricardo Martin Gonzalez Diaz <richimartin99@users.noreply.github.com>
2021-01-24 23:56:48 -0600

    cambio

--- serviciosapp/views.py
+++ serviciosapp/views.py
@@ -7,24 +7,6 @@ from datetime import datetime,date
 
 # Create your views here.
 
-def login(request):
-    matricula=request.POST['matricula']
-    contrasenia=request.POST['contrasenia']
-    usuarios = usuario.objects.all()
-    for usuario in usuarios:
-        if usuario.matricula == matricula and usuario.contrasenia == contrasenia:
-            u = usuario
-    context = {
-        'usuario':u
-    }
-    return render(request, 'datos_solicitante.html', context)
-
-#def registrar(request):
-#    u = usuario(request.POST['matricula'], request.POST['contrasenia'], request.POST['email'])
-#    u.save()
-#    return render(request, 'index.html')
-
-
 def obtenerDatos_Solicitante(request):
 
     #consulta_datosPersonales = datosPersonales.objects.get()
