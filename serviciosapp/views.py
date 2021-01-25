c0757ab7767899e6ec0e867a40a8415d6b454297
eroseli <48666884+eroseli@users.noreply.github.com>
2021-01-25 04:33:03 -0600

    Cambios

--- serviciosapp/views.py
+++ serviciosapp/views.py
@@ -27,26 +27,17 @@ def login(request):
 
 def obtenerDatos_Solicitante(request):
 
-    if request.POST['solicita_beca_alimentaria'] == 'si':
-        variable = True
-    else :
-        variable = False
-    if request.POST['residencia_distinta'] == 'si':
-        variable2 = True
-    else :
-        variable2 = False
-    if request.POST['sexo'] == "M":
-        variable3 = True
-    else :
-        variable3 = False
+    consulta_datosPersonales = datosPersonales.objects.get()
 
-    datosPersonalesModel = datosPersonales(fecha = date.today(), solicita_beca_alimentaria= bool(variable),
+    datosPersonalesModel = datosPersonales(fecha = date.today(), solicita_beca_alimentaria= request.POST['solicita_beca_alimentaria'],
     ap_paterno=request.POST['apellido_paterno'], ap_materno = request.POST['apellido_materno'], nombre = request.POST['nombre'],
-    sexo = variable3, edad = request.POST['edad'], estado_civil = request.POST['estado_civil'], carrera =request.POST['carrera'],
+    sexo = request.POST['sexo'], edad = request.POST['edad'], estado_civil = request.POST['estado_civil'], carrera =request.POST['carrera'],
     semestre = request.POST['semestre'],grupo = request.POST['grupo'],telefono = request.POST['celular'], otro_idiona =  request.POST['otro_idioma'],
-    residencia_distinta = variable2 , calle =  request.POST['calle'], numero = request.POST['numero_calle'],
+    residencia_distinta = request.POST['residencia_distinta'] , calle =  request.POST['calle'], numero = request.POST['numero_calle'],
     colonia = request.POST['barrio'], municipio = request.POST['municipio'], estado = request.POST['estado'],propietario = request.POST['propietario'],
     parentesco = request.POST['parentesco'])
+
+    datosPersonales.save()
 
     context = {
         'datosPersonalesModel': datosPersonalesModel,
